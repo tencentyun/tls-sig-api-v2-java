74e5b3bc84c27f46e702cc875747bb002db33137
yutingzeng <yutingzeng@tencent.com>
2020-09-11 12:00:47 +0800

    增加使用字符串房间号生成privateMapKey  '#' will be ignored, and an empty message aborts the commit.

--- src/main/java/com/tencentyun/TLSSigAPIv2.java
+++ src/main/java/com/tencentyun/TLSSigAPIv2.java
@@ -52,20 +52,51 @@ public class TLSSigAPIv2 {
      * @param expire       - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
      * @param roomid       - 房间号，用于指定该 userid 可以进入的房间号
      * @param privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
-     * - 第 1 位：0000 0001 = 1，创建房间的权限
-     * - 第 2 位：0000 0010 = 2，加入房间的权限
-     * - 第 3 位：0000 0100 = 4，发送语音的权限
-     * - 第 4 位：0000 1000 = 8，接收语音的权限
-     * - 第 5 位：0001 0000 = 16，发送视频的权限
-     * - 第 6 位：0010 0000 = 32，接收视频的权限
-     * - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
-     * - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限
-     * - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
-     * - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
+     *                     - 第 1 位：0000 0001 = 1，创建房间的权限
+     *                     - 第 2 位：0000 0010 = 2，加入房间的权限
+     *                     - 第 3 位：0000 0100 = 4，发送语音的权限
+     *                     - 第 4 位：0000 1000 = 8，接收语音的权限
+     *                     - 第 5 位：0001 0000 = 16，发送视频的权限
+     *                     - 第 6 位：0010 0000 = 32，接收视频的权限
+     *                     - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
+     *                     - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限
+     *                     - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
+     *                     - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
      * @return usersig - 生成带userbuf的签名
      */
     public String genPrivateMapKey(String userid, long expire, long roomid, long privilegeMap) {
-        byte[] userbuf = genUserBuf(userid, roomid, expire, privilegeMap, 0);  //生成userbuf
+        byte[] userbuf = genUserBuf(userid, roomid, expire, privilegeMap, 0, null);  //生成userbuf
+        return genUserSig(userid, expire, userbuf);
+    }
+
+    /**
+     * 【功能说明】
+     * 用于签发 TRTC 进房参数中可选的 PrivateMapKey 权限票据。
+     * PrivateMapKey 需要跟 UserSig 一起使用，但 PrivateMapKey 比 UserSig 有更强的权限控制能力：
+     * - UserSig 只能控制某个 UserID 有无使用 TRTC 服务的权限，只要 UserSig 正确，其对应的 UserID 可以进出任意房间。
+     * - PrivateMapKey 则是将 UserID 的权限控制的更加严格，包括能不能进入某个房间，能不能在该房间里上行音视频等等。
+     * 如果要开启 PrivateMapKey 严格权限位校验，需要在【实时音视频控制台】/【应用管理】/【应用信息】中打开“启动权限密钥”开关。
+     * <p>
+     * 【参数说明】
+     *
+     * @param userid       - 用户id，限制长度为32字节，只允许包含大小写英文字母（a-zA-Z）、数字（0-9）及下划线和连词符。
+     * @param expire       - PrivateMapKey 票据的过期时间，单位是秒，比如 86400 生成的 PrivateMapKey 票据在一天后就无法再使用了。
+     * @param roomstr      - 字符串房间号，用于指定该 userid 可以进入的房间号
+     * @param privilegeMap - 权限位，使用了一个字节中的 8 个比特位，分别代表八个具体的功能权限开关：
+     *                     - 第 1 位：0000 0001 = 1，创建房间的权限
+     *                     - 第 2 位：0000 0010 = 2，加入房间的权限
+     *                     - 第 3 位：0000 0100 = 4，发送语音的权限
+     *                     - 第 4 位：0000 1000 = 8，接收语音的权限
+     *                     - 第 5 位：0001 0000 = 16，发送视频的权限
+     *                     - 第 6 位：0010 0000 = 32，接收视频的权限
+     *                     - 第 7 位：0100 0000 = 64，发送辅路（也就是屏幕分享）视频的权限
+     *                     - 第 8 位：1000 0000 = 200，接收辅路（也就是屏幕分享）视频的权限
+     *                     - privilegeMap == 1111 1111 == 255 代表该 userid 在该 roomid 房间内的所有功能权限。
+     *                     - privilegeMap == 0010 1010 == 42  代表该 userid 拥有加入房间和接收音视频数据的权限，但不具备其他权限。
+     * @return usersig - 生成带userbuf的签名
+     */
+    public String genPrivateMapKeyWithStringRoomID(String userid, long expire, String roomstr, long privilegeMap) {
+        byte[] userbuf = genUserBuf(userid, 0, expire, privilegeMap, 0, roomstr);  //生成userbuf
         return genUserSig(userid, expire, userbuf);
     }
 
@@ -125,7 +156,7 @@ public class TLSSigAPIv2 {
     }
 
     public byte[] genUserBuf(String account, long dwAuthID, long dwExpTime,
-                             long dwPrivilegeMap, long dwAccountType) {
+                             long dwPrivilegeMap, long dwAccountType, String RoomStr) {
         //视频校验位需要用到的字段,按照网络字节序放入buf中
         /*
          cVer    unsigned char/1 版本号，填0
@@ -138,11 +169,20 @@ public class TLSSigAPIv2 {
          dwAccountType   unsigned int/4  第三方帐号类型
          */
         int accountLength = account.length();
+        int roomStrLength = RoomStr.length();
         int offset = 0;
-        byte[] userbuf = new byte[1 + 2 + accountLength + 4 + 4 + 4 + 4 + 4];
+        int bufLength = 1 + 2 + accountLength + 20 ;
+        if (roomStrLength > 0) {
+            bufLength = bufLength + 2 + roomStrLength;
+        }
+        byte[] userbuf = new byte[bufLength];
 
         //cVer
-        userbuf[offset++] = 0;
+        if (roomStrLength > 0) {
+            userbuf[offset++] = 1;
+        } else {
+            userbuf[offset++] = 0;
+        }
 
         //wAccountLen
         userbuf[offset++] = (byte) ((accountLength & 0xFF00) >> 8);
@@ -185,6 +225,17 @@ public class TLSSigAPIv2 {
         userbuf[offset++] = (byte) ((dwAccountType & 0x0000FF00) >> 8);
         userbuf[offset++] = (byte) (dwAccountType & 0x000000FF);
 
+
+        if (roomStrLength > 0) {
+            //roomStrLen
+            userbuf[offset++] = (byte) ((roomStrLength & 0xFF00) >> 8);
+            userbuf[offset++] = (byte) (roomStrLength & 0x00FF);
+
+            //roomStr
+            for (; offset < bufLength; ++offset) {
+                userbuf[offset] = (byte) RoomStr.charAt(offset - (bufLength - roomStrLength));
+            }
+        }
         return userbuf;
     }
 }
